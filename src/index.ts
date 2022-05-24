3c5de150ffde5c1b7c730b84a2ceed0c73960121
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-24 20:16:08 +0200

    basic healthcheck message

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const serverStartMsg = "Express server started on port: ";
 const port = process.env.PORT || 3000;
 
 // Start server
-const runningServer = server.listen(port, () =>{});
+const runningServer = server.listen(port, () =>{
+    console.log("Environment: " + process.env.NODE_ENV);
+    console.log("Express Server started on port: " + port);
+});
 
 export default runningServer;
