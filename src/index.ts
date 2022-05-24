2f82e55d7802ee2481816d1f4706de1bf43e5c98
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-25 01:00:59 +0200

    set the versiong

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 // Start server
 const runningServer = server.listen(port, () =>{
+    console.log("v1:00");
     console.log("Environment: " + process.env.NODE_ENV);
     console.log("Redis Url: " +   process.env.REDIS_URL);
     console.log("Express Server started on port: " + port);
