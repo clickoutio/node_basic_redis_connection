d90ab533a6b64bc2820981390020d3d156712f60
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-24 20:08:09 +0200

    create basic server

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import apiRouter from "./api";
+
+//import apiRouter from "./api";
+
+// Constants
+const app = express();
+
+
+/***********************************************************************************
+ *                                  Middlewares
+ **********************************************************************************/
+
+// Common middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+/***********************************************************************************
+ *                               Configure Routes
+ **********************************************************************************/
+// Show routes called in console during development
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Security (helmet recommended in express docs)
+if (process.env.NODE_ENV === "production") {
+  app.use(helmet());
+}
+
+/***********************************************************************************
+ *                         API routes and error handling
+ **********************************************************************************/
+app.use("/", apiRouter);
+
+
+// Export here and start in a diff file (for testing).
+export default app;
