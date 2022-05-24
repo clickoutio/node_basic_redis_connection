c2bb2365a3e4224ca8cec0c9fb6c7a588df80240
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-24 20:30:14 +0200

    create endpoint that calls redisConnector

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
+import { makeAPingInRedis } from "./redisConnector";
 
 const baseRouter = Router();
+
 baseRouter.get("/", (req, res)=> res.send("que pasa"));
+
+baseRouter.get("/testRedisConnection", async (req, res) => {
+
+    const text = await makeAPingInRedis();
+
+    res.send(text);
+});
 
 export default baseRouter;
