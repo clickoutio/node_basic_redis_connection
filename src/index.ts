49e92abb77e6d6b9a370f13f6325e854ec2e8665
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-25 10:57:27 +0200

    changed version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 // Start server
 const runningServer = server.listen(port, () =>{
-    console.log("v10:05");
+    console.log("v10:57");
     console.log("Environment: " + process.env.NODE_ENV);
     console.log("Redis host: " +   process.env.REDIS_HOST);
     console.log("Express Server started on port: " + port);
