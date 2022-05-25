40973db6981cf8393249937c2beca156f4a51d90
Javier Morales de Vera <javiermoralesdevera99@gmail.com>
2022-05-25 09:20:49 +0200

    new try

--- src/redisConnector.ts
+++ src/redisConnector.ts
@@ -2,18 +2,17 @@ import Redis from "ioredis";
 
 export async function makeAPingInRedis(){
 
-    const cluster = new Redis.Cluster([
-        {
-            host: '127.0.0.1',
-            port: 6379
-        }
-    ]);
+    const client = new Redis({
+        host: process.env.REDIS_HOST,
+        port: 6379
+    });
 
     //await client.connect();
-    await cluster.set("value", "Connection stablished");
-    const message = await cluster.get("value");
+    await client.set("value", "Connection stablished");
+    const message = await client.get("value");
     //await client.quit();
 
     console.log(message);
     return message;
+    return "didnt failed";
 }
